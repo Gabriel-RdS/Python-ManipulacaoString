652a6469895c9a1fe30ba7fb8ab595d99ef73979
Gabriel Ramos dos Santos <gabrielramosds@outlook.com>
2020-12-11 15:51:18 -0300

    Adicionando classe ExtratorArgumentosURL

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from ExtratorArgumentosURL import ExtratorArgumentosURL
 
+"""
 url = "www.bytebank.com.br/cambio?`valor=1500&moedaOrigem=real&moedaDestino=dolar"
 indice = url.find("?")
 print(url[indice + 1:])
+
+"""
+
+url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dólar"
+
+argumentosUrl = ExtratorArgumentosURL(url)
+
+moedaOrigem, MoedaDestino = argumentosUrl.retornaMoedas()
+print(moedaOrigem, MoedaDestino)
